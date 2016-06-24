79464c681638532ed97168388bfa5bd516a3b17f
Reini Urban <rurban@cpanel.net>
2016-06-24 13:56:38 +0200

    0.017: simplify handler, load backends on demand
    less memory and run-time.
    load the backend modules only when needed.

--- lib/File/StripNondeterminism.pm
+++ lib/File/StripNondeterminism.pm
@@ -19,82 +19,93 @@
 package File::StripNondeterminism;
 
 use strict;
-use warnings;
-
-use File::StripNondeterminism::handlers::ar;
-use File::StripNondeterminism::handlers::gzip;
-use File::StripNondeterminism::handlers::jar;
-use File::StripNondeterminism::handlers::javadoc;
-use File::StripNondeterminism::handlers::pearregistry;
-use File::StripNondeterminism::handlers::png;
-use File::StripNondeterminism::handlers::javaproperties;
-use File::StripNondeterminism::handlers::zip;
-
 our($VERSION, $canonical_time);
-
-$VERSION = '0.015'; # 0.015
+$VERSION = '0.017'; # 0.017
 
 sub _get_file_type {
-	my $file=shift;
-	open (FILE, '-|') # handle all filenames safely
-		|| exec('file', $file)
-		|| die "can't exec file: $!";
-	my $type=<FILE>;
-	close FILE;
-	return $type;
+    my $file=shift;
+    open (FILE, '-|') # handle all filenames safely
+      || exec('file', $file)
+      || die "can't exec file: $!";
+    my $type=<FILE>;
+    close FILE;
+    return $type;
 }
 
 sub get_normalizer_for_file {
-	$_ = shift;
-
-	return undef if -d $_; # Skip directories
-
-	# ar
-	if (m/\.a$/ && _get_file_type($_) =~ m/ar archive/) {
-		return \&File::StripNondeterminism::handlers::ar::normalize;
-	}
-	# gzip
-	if (m/\.(gz|dz)$/ && _get_file_type($_) =~ m/gzip compressed data/) {
-		return \&File::StripNondeterminism::handlers::gzip::normalize;
-	}
-	# jar
-	if (m/\.(jar|war|hpi)$/ && _get_file_type($_) =~ m/(Java|Zip) archive data/) {
-		return \&File::StripNondeterminism::handlers::jar::normalize;
-	}
-	# javadoc
-	if (m/\.html$/ && File::StripNondeterminism::handlers::javadoc::is_javadoc_file($_)) {
-		return \&File::StripNondeterminism::handlers::javadoc::normalize;
-	}
-	# pear registry
-	if (m/\.reg$/ && File::StripNondeterminism::handlers::pearregistry::is_registry_file($_)) {
-		return \&File::StripNondeterminism::handlers::pearregistry::normalize;
-	}
-	# PNG
-	if (m/\.png$/ && _get_file_type($_) =~ m/PNG image data/) {
-		return \&File::StripNondeterminism::handlers::png::normalize;
-	}
-	# pom.properties, version.properties
-	if (m/(pom|version)\.properties$/ && File::StripNondeterminism::handlers::javaproperties::is_java_properties_file($_)) {
-		return \&File::StripNondeterminism::handlers::javaproperties::normalize;
-	}
-	# zip
-	if (m/\.(zip|pk3|whl|xpi)$/ && _get_file_type($_) =~ m/Zip archive data/) {
-		return \&File::StripNondeterminism::handlers::zip::normalize;
-	}
-	return undef;
+    $_ = shift;
+    return undef if -d $_; # Skip directories
+    my $map = [
+        sub {
+            $_ = shift;
+            m/\.a$/ && _get_file_type($_) =~ m/ar archive/
+        } => 'ar',
+        sub {
+            $_ = shift;
+            m/\.g?mo$/ && _get_file_type($_) =~ m/GNU message catalog/
+        } => 'gettext',
+        sub {
+            $_ = shift;
+            m/\.(gz|dz)$/ && _get_file_type($_) =~ m/gzip compressed data/;
+        } => 'gzip',
+        sub { 
+            $_ = shift;
+            m/\.(jar|war|hpi)$/ && _get_file_type($_) =~ m/(Java|Zip) archive data/;
+        } => 'jar',
+        sub {
+            $_ = shift;
+            if (m/\.html$/) {
+                require File::StripNondeterminism::handlers::javadoc;
+                return File::StripNondeterminism::handlers::javadoc::is_javadoc_file($_) : 1 : 0;
+            } else {
+                0
+            };
+        } => 'javadoc',
+        sub {
+            $_ = shift;
+            if (m/\.reg$/) {
+                require File::StripNondeterminism::handlers::pearregistry;
+                return File::StripNondeterminism::handlers::pearregistry::is_registry_file($_) ? 1 : 0;
+            } else {
+                0
+            };
+        } => 'pearregistry',
+        sub {
+            $_ = shift;
+            m/\.png$/ && _get_file_type($_) =~ m/PNG image data/;
+        } => 'png',
+        sub {
+            $_ = shift;
+            # pom.properties, version.properties
+            if (m/(pom|version)\.properties$/) {
+		require File::StripNondeterminism::handlers::javaproperties;
+                return File::StripNondeterminism::handlers::javaproperties::is_java_properties_file($_);
+                }
+	} => 'javaproperties',
+        sub {
+            $_ = shift;
+            m/\.(zip|pk3|whl|xpi)$/ && _get_file_type($_) =~ m/Zip archive data/;
+        } => 'zip',
+      ];
+    
+    for my $m (@$map) {
+        if ($m->[0]($_)) {
+            no strict 'refs';
+            my $p = $m->[1]; 
+            eval { eval "require File::StripNondeterminism::handlers::".$p; };
+            return undef if $@;
+            return \&{"File::StripNondeterminism::handlers::".$p."::normalize"};
+        }
+    }
+    return undef;
 }
 
 sub get_normalizer_by_name {
-	$_ = shift;
-	return \&File::StripNondeterminism::handlers::ar::normalize if $_ eq 'ar';
-	return \&File::StripNondeterminism::handlers::gzip::normalize if $_ eq 'gzip';
-	return \&File::StripNondeterminism::handlers::jar::normalize if $_ eq 'jar';
-	return \&File::StripNondeterminism::handlers::javadoc::normalize if $_ eq 'javadoc';
-	return \&File::StripNondeterminism::handlers::pearregistry::normalize if $_ eq 'pearregistry';
-	return \&File::StripNondeterminism::handlers::png::normalize if $_ eq 'png';
-	return \&File::StripNondeterminism::handlers::javaproperties::normalize if $_ eq 'javaproperties';
-	return \&File::StripNondeterminism::handlers::zip::normalize if $_ eq 'zip';
-	return undef;
+    no strict 'refs';
+    $_ = shift;
+    eval { eval "require File::StripNondeterminism::handlers::".$_; };
+    return undef if $@;
+    return \&{"File::StripNondeterminism::handlers::".$_."::normalize"};
 }
 
 1;
